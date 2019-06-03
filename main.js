de8c8f50b3577a8df51fdf34b09519b484fb6dc4
Daniel Palencia <Lionheart920@hotmail.com>
2019-06-03 13:37:17 -0700

    commit #1 6-3-19

--- main.js
+++ main.js
@@ -1,77 +1,116 @@
-function hello(){
-    console.log('hello');
-}
-hello();
-
-
-
-/*
-const questions = [
-    {
-        question: 'What is the name of Ariel\'s prince?',
-        answers:['Prince Philip','Prince Naveen','Prince Eric','Prince Ali','Prince Gabo'],
-        correctAnswer: 2
-    },
-    {
-        question: 'What is the name of the female mouse in \'The Rescuers\'?',
-        answers:['Bianca','Bernadette','Brittany','Bridget','Gabo'],
-        correctAnswer: 0
-    },
-    {
-        question: 'Who is the villian in \'Sleeping Beauty\'?',
-        answers:['Ursala','Malificent','Hades','Cuella De Vil','Gabo'],
-        correctAnswer: 1
-    },
-    {
-        question: 'What Character says they have a snake in their boot in Toy Story?',
-        answers:['Buzz','Boo Peep','Woody','Mr. Potato Head','Gabo'],
-        correctAnswer: 2
-    },
-    {
-        question: 'Which character from Disney\'s Pixar \'Finding Nemo\' suffers from short term memory loss?',
-        answers:['Marlin','Nemo','Dory','Bruce','Gabo'],
-        correctAnswer: 2
-    },
-    {
-        question: 'From Disney\'s \'UP\'What type of soda is the Ellie Badge?',
-        answers:['Orange','Grape','Cherry','Lime','Gabo'],
-        correctAnswer: 1
-    },
-    {
-        question: 'How many dogs do the dogs Pongo and Purdy have?',
-        answers:['101','15','7','10','Gabo'],
-        correctAnswer: 0
-    },
-    {
-        question: 'What is the name of the dog from \'COCO\'?',
-        answers:['Chico','Dante','Doug','Pongo','Gabo'],
-        correctAnswer: 1
-    },
-    {
-        question: 'What is the phrase Timon and Pumba taught Simba from \'Lion King\'?',
-        answers:['Hakuna Matata','No Way Jose!','You have our blessing','Kill the beast','Gabo Gabo'],
-        correctAnswer: 0 
-    },
-    {
-        question: 'Which one of these is NOT a 7 dwarf?',
-        answers:['Dopey','Happy','Sleepy','Doc','Gabo'],
-        correctAnswer: 4 
-    }
-];
 let score = 0;
 let currentQuestion = 0;
+const questions = [
+    {
+        title: 'What is the name of Ariel\'s prince?',
+        answers:['Prince Philip','Prince Naveen','Prince Eric','Prince Ali','Prince Gabo'],
+        correct: 2
+    },
+    {
+        title: 'What is the name of the female mouse in \'The Rescuers\'?',
+        answers:['Bianca','Bernadette','Brittany','Bridget','Gabo'],
+        correct: 0
+    },
+    {
+        title: 'Who is the villian in \'Sleeping Beauty\'?',
+        answers:['Ursala','Malificent','Hades','Cuella De Vil','Gabo'],
+        correct: 1
+    },
+    {
+        title: 'What Character says they have a snake in their boot in Toy Story?',
+        answers:['Buzz','Boo Peep','Woody','Mr. Potato Head','Gabo'],
+        correct: 2
+    },
+    {
+        title: 'Which character from Disney\'s Pixar \'Finding Nemo\' suffers from short term memory loss?',
+        answers:['Marlin','Nemo','Dory','Bruce','Gabo'],
+        correct: 2
+    },
+    {
+        title: 'From Disney\'s \'UP\'What type of soda is the Ellie Badge?',
+        answers:['Orange','Grape','Cherry','Lime','Gabo'],
+        correct: 1
+    },
+    {
+        title: 'How many dogs do the dogs Pongo and Purdy have?',
+        answers:['101','15','7','10','Gabo'],
+        correct: 0
+    },
+    {
+        title: 'What is the name of the dog from \'COCO\'?',
+        answers:['Chico','Dante','Doug','Pongo','Gabo'],
+        correct: 1
+    },
+    {
+        title: 'What is the phrase Timon and Pumba taught Simba from \'Lion King\'?',
+        answers:['Hakuna Matata','No Way Jose!','You have our blessing','Kill the beast','Gabo Gabo'],
+        correct: 0 
+    },
+    {
+        title: 'Which one of these is NOT a 7 dwarf?',
+        answers:['Dopey','Happy','Sleepy','Doc','Gabo'],
+        correct: 4 
+    }
+];
 
-function startQuiz() {
-    
+ $('.start a').on('click', function(e){
+    e.preventDefault();
+    $('.start').hide();
+    $('.quiz').show();
+    showQuestion();
+});
+
+$('.quiz ul').on('click', 'li', function(){
+    $('.selected').removeClass()
+    $(this).toggleClass('selected')
+})
+$('.quiz a').click(function(e){
+    e.preventDefault();
+    if($('li.selected').length){
+        let guess = parseInt($('li.selected').attr('id'));
+        checkAnswer(guess);        
+    } else {
+        alert('Please select an answer')
+    }
+$('.summary a').click(function(e){
+     e.preventDefault();
+    restartQuiz();
+    });
+
+})
+function updateQuestionNumber() {
+    let question = questions[currentQuestion];
 }
+
 function showQuestion() {
-    
+    let question = questions[currentQuestion];
+    $('.quiz h2').text(question.title);
+    $('.quiz ul').html('');
+    for(let i=0; i<question.answers.length; i++){
+        $('.quiz ul').append(`<li id="${i}">${question.answers[i]}</li>`);
+    }
 }
-function checkAnswer() {
-    
+function checkAnswer(guess) {
+    let question = questions[currentQuestion];
+    if(question.correct === guess){
+        score++;
+    }
+    currentQuestion++;
+    if(currentQuestion >= questions.length ){
+        showSummary();
+    } else {
+    showQuestion();
+    }
 }
 function showSummary(){
-
+    $('.quiz').hide();
+    $('.summary').show();
+    $('.summary p').text('You got ' +score+ ' out of ' +questions.length+' correct!');
 }
-
-*/
+function restartQuiz(){
+    $('.summary').hide();
+    $('.quiz').show();
+    score = 0;
+    currentQuestion = 0;
+    showQuestion();
+};
